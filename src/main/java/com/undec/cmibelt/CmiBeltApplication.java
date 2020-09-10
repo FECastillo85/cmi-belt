12a49adc15223eb056bcbb0439c13ab51ed1d781
FECastillo85 <56367678+FECastillo85@users.noreply.github.com>
2020-09-10 01:31:09 -0300

    Proyecto inicial. Estructura inicial

---
+++ src/main/java/com/undec/cmibelt/CmiBeltApplication.java
@@ -0,0 +1,13 @@
+package com.undec.cmibelt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmiBeltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmiBeltApplication.class, args);
+	}
+
+}
